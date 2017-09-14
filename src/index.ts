60f541f94e5c589a767e4ebcb2e6946f383f9fa4
jiewj <jie_wj@126.com>
2017-09-14 14:34:48 +0800

    emoji

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,42 @@
 require('./index.scss');
 
+let xhr = new XMLHttpRequest();
+xhr.open('get', './emoji.json', true);
+
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            emojiHtml(xhr.response);
+        }
+    }
+};
+xhr.send();
+
+function emojiHtml(response: any) {
+    const emoji = JSON.parse(response);
+    let emojiTable = document.getElementById('emojiTable');
+    let emojiTr = '', emojiTd = '';
+    emoji.forEach((value: any) => {
+        if ((emoji.indexOf(value) + 1) % 15 == 0) {
+            emojiTr += '<tr>' + emojiTd + '</tr>';
+            emojiTd = '';
+        } else {
+            emojiTd += `
+    <td>
+    <span class="editor-emoji-table-td" data-name="${value.name}" style="background-image: url(${value.base64})"></span>
+</td>`;
+            if (emoji.length == emoji.indexOf(value) + 1) {
+                emojiTr += '<tr>' + emojiTd + '</tr>';
+            }
+        }
+    });
+    emojiTable.innerHTML = '<table class="editor-emoji-table">' + emojiTr + '</table>';
+}
+
 
 let editorContent = document.getElementById('editorContent');
 
-function execCommandFun(aCommandName:string){
+function execCommandFun(aCommandName: string) {
     document.execCommand(aCommandName, false, null);
     editorContent.focus();
 }
