75056777447295b4e1fdedd566c40fba9ba9201f
jiewj <jie_wj@126.com>
2017-09-15 17:56:18 +0800

    dev

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,7 @@ function imageFileFun(obj: any) {
     for (let i = 0; i < files.length; i++) {
         console.dir(files[i]);
         console.log(URL.createObjectURL(files[i]));
-        html += `<img src="${URL.createObjectURL(files[i])}"/>`;
+        html += `<img class="editor-content-img" src="${URL.createObjectURL(files[i])}"/>`;
     }
     execCommandFun('insertHTML', html);
 }
@@ -65,12 +65,64 @@ function videoFileFun(obj: any) {
 
 let editorContent = document.getElementById('editorContent');
 
+editorContent.addEventListener('click', function (e) {
+    let target = e.target as HTMLElement;
+    // let type = document.querySelectorAll('[data-type="editor"]');
+
+    console.dir(target);
+    let checked = {'I': 'italic','B': 'bold','U': 'underline','LI': 'insertUnorderedList'};
+    let hasTag:any = [];
+
+/*    for (let i = 0; i < type.length; i++) {
+        if ((type[i] as HTMLInputElement).checked) {
+            switch ((type[i] as HTMLInputElement).defaultValue) {
+                case 'italic':
+                    checked = Object.assign({}, checked, {'I': 'italic'});
+                    break;
+                case 'bold':
+                    checked = Object.assign({}, checked, {'B': 'bold'});
+                    break;
+                case 'underline':
+                    checked = Object.assign({}, checked, {'U': 'underline'});
+                    break;
+                case 'insertUnorderedList':
+                    checked = Object.assign({}, checked, {'LI': 'insertUnorderedList'});
+                    break;
+                default:
+                    break;
+            }
+        }
+    }*/
+
+    nodeName(target);
+
+    console.log(hasTag);
+    hasTag.forEach((value:any)=>{
+       console.log(value);
+       console.log(checked[value]);
+        let type = document.querySelectorAll(`[data-type="${checked[value]}"]`)[0] as HTMLInputElement;
+        type.checked = true;
+
+        console.log(type);
+    });
+
+    function nodeName(obj: any) {
+        if (obj.nodeName in checked) {
+            hasTag.push(obj.nodeName);
+            nodeName(obj.parentNode);
+        }
+
+    }
+});
+
+
 function execCommandFun(aCommandName: string, aValueArgument: any = null) {
     editorContentFocus();
     document.execCommand(aCommandName, false, aValueArgument);
 }
-function editorContentFocus(){
+function editorContentFocus() {
     editorContent.focus();
+
 }
 
 (<any> window).editorContentFocus = editorContentFocus;
@@ -81,12 +133,13 @@ function editorContentFocus(){
 (<any> window).videoFileFun = videoFileFun;
 (<any> window).addLink = addLink;
 
+let editorContentBox = document.getElementsByClassName('editor-content-box')[0];
 
 let editorScrollTop = document.getElementsByClassName('editor-scroll-top')[0] as HTMLElement;
-editorContent.addEventListener('scroll',function () {
-    let top = Math.trunc((this.scrollTop) / this.scrollHeight * this.clientHeight);
-    let topMax = Math.trunc((this.scrollHeight - this.clientHeight) / this.scrollHeight * this.clientHeight);
-    let height = Math.trunc((this.clientHeight - topMax) / this.clientHeight * 1000) /1000;
+editorContentBox.addEventListener('scroll', function () {
+    let top = Math.trunc((this.scrollTop) / this.scrollHeight * editorContentBox.clientHeight);
+    let topMax = Math.trunc((this.scrollHeight - editorContentBox.clientHeight) / this.scrollHeight * editorContentBox.clientHeight);
+    let height = Math.trunc((editorContentBox.clientHeight - topMax) / editorContentBox.clientHeight * 1000) / 1000;
     editorScrollTop.style.transform = `translateY(${top}px) scaleY(${height})`;
 });
 
@@ -94,12 +147,12 @@ let emojiIcon = document.getElementsByClassName('emojiIcon')[0] as HTMLInputElem
 let iframeLink = document.getElementsByClassName('iframeLink')[0] as HTMLInputElement;
 
 
-document.addEventListener('click',function (e) {
+document.addEventListener('click', function (e) {
     let target = e.target as HTMLElement;
-    if(target.className != 'emojiIcon'){
+    if (target.className != 'emojiIcon') {
         emojiIcon.checked = false;
     }
-    if(target.className != 'iframeLink'){
+    if (target.className != 'iframeLink') {
         iframeLink.checked = false;
     }
 
